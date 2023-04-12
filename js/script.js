5e0056711dc8c3a0e979284ebca475ba39e1e6ca
jailcomfranssa <jail_son_0@hotmail.com>
2023-04-12 12:07:13 -0300

    add js e css no html

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/*=====================typing animation======================= */
+
+var typed = new Typed(".typing",{
+    strings:["","Desenvolvedor Full Stack","Web Designer","Web Developer", "Front end Developer","Back end Developer","QA Software Engineer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+/*============================================ */
